32243c1c1940892e34fb086fdd05e70bfa0ad81e
StokeBreakup <165694895+stokebreakup@users.noreply.github.com>
2024-06-01 18:35:29 -0300

    add create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,6 @@
+-- criando tabela para registrar venda de tênis por nome da marca, tipo de tênis, ano do tênis
+CREATE TABLE VendasTenis (
+  nome_da_marca VARCHAR(255),
+  tipo_de_tenis VARCHAR(255),
+  ano_do_tenis INT
+);
